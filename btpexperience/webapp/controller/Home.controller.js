055d428ea0e191ca9012174ae3a9d49492c7e6ec
raqmarangoni <rmarangonisan@gmail.com>
2023-12-08 13:22:17 -0300

    feat: dialog fragment

--- btpexperience/webapp/controller/Home.controller.js
+++ btpexperience/webapp/controller/Home.controller.js
@@ -1,7 +1,7 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
-    "sap/m/MessageToast"
+    "sap/m/MessageToast",
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
@@ -21,54 +21,86 @@ sap.ui.define([
                         {
                             Empresa: "Empresa A",
                             Estado: "São Paulo",
-                            Logo: "../img/empresaA.png"
+                            Logo: "../img/empresa.png"
                         },
                         {
                             Empresa: "Empresa B",
                             Estado: "São Paulo",
-                            Logo: "../img/empresaA.png"
+                            Logo: "../img/empresa.png"
                         },
                         {
                             Empresa: "Empresa C",
                             Estado: "São Paulo",
-                            Logo: "../img/empresaA.png"
+                            Logo: "../img/empresa.png"
                         },
                         {
                             Empresa: "Empresa D",
                             Estado: "São Paulo",
-                            Logo: "../img/empresaA.png"
+                            Logo: "../img/empresa.png"
                         },
                         {
                             Empresa: "Empresa E",
                             Estado: "São Paulo",
-                            Logo: "../img/empresaA.png"
+                            Logo: "../img/empresa.png"
                         },
                         {
                             Empresa: "Empresa F",
                             Estado: "São Paulo",
-                            Logo: "../img/empresaA.png"
+                            Logo: "../img/empresa.png"
                         },
                         {
                             Empresa: "Empresa G",
                             Estado: "São Paulo",
-                            Logo: "../img/empresaA.png"
+                            Logo: "../img/empresa.png"
+                        },
+                        {
+                            Empresa: "Empresa H",
+                            Estado: "São Paulo",
+                            Logo: "../img/empresa.png"
                         },
                     ]
                 };
                 const oModel = new JSONModel(patrocinadoresModel);
                 this.getView().setModel(oModel);
+
+                const oPackageModel = new JSONModel({ Nome: "Diamante", Preco: "10000,00", Convite: "15", GrupoMajoritario: "A", Stand: "Grande" })
+                this.getView().setModel(oPackageModel, "packageInfo");
             },
+
             onPress: function () {
                 const message = 'pressed';
                 MessageToast.show(message);
             },
+
             onOpenDialog() {
-                
                 this.pDialog ??= this.loadFragment({
-                    name: "com.lab2dev.btpexperience.view.HelloDialog"
+                    name: "com.lab2dev.btpexperience.view.HelloDialog",
+                    controller: this
                 });
-            
-                this.pDialog.then((oDialog) => oDialog.open());
-            }
+
+                this.pDialog.then(oDialog => {
+                    this.getView().insertDependent(oDialog)
+                    const oData = this.getView().getModel("packageInfo").getData()
+                    const oModel = new JSONModel(oData)
+                    this.getView().setModel(oModel, "packageForm")
+                    oDialog.open()
+                });
+            },
+            onSendPackage: function () {
+                const oData = this.getView().getModel("packageForm").getData()
+                const oModel = this.getView().getModel("packageInfo")
+                oModel.setData(oData);
+                this.byId("packageFormDialog").close();
+            },
+            onCancelChanges: function () {
+                const oOriginalData = this.getView().getModel("packageInfo").getData();
+                const oModel = new JSONModel(oOriginalData);
+                this.getView().setModel(oModel, "packageForm");
+
+                // Fechar o diálogo, se estiver aberto
+                if (this.byId("packageFormDialog").isOpen()) {
+                    this.byId("packageFormDialog").close();
+                }
+            },
         });
     });
